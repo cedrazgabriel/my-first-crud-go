3d5bce53dcd095767ecda9b4bfc46c99ba3d825d
Gabriel Cedraz <cedrazdev@gmail.com>
2025-05-03 16:58:40 -0300

    refactor: integra o framework Gin e ajusta a criação de usuários com tratamento de erros

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/cedrazgabriel/my-first-crud-go/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
-
+	err := rest_err.NewBadRequestError("error creating user")
+	c.JSON(err.Code, err)
 }
