c38b83b05696ffa7002a643411018b30d3e0da6e
Gabriel Cedraz <cedrazdev@gmail.com>
2025-05-04 16:39:02 -0300

    feat: adiciona logging para criação de usuários e inicialização do servidor

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/cedrazgabriel/my-first-crud-go/src/configuration/logger"
 	"github.com/cedrazgabriel/my-first-crud-go/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Starting server...")
 	err := godotenv.Load()
 
 	if err != nil {
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,22 +1,30 @@
 package controller
 
 import (
-	"fmt"
-
+	"github.com/cedrazgabriel/my-first-crud-go/src/configuration/logger"
 	"github.com/cedrazgabriel/my-first-crud-go/src/configuration/validation"
 	"github.com/cedrazgabriel/my-first-crud-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Creating user...",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error binding JSON", err,
+			zap.String("journey", "createUser"),
+		)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
 
 }
